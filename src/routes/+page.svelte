7051f865f004e4c21af7d4e5960df74ee47f5687
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2026-01-18 00:44:47 +0100

    portfolio adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,22 +10,20 @@
 
 <svelte:head>
 	<title>Profile card</title>
-	<link rel="preload" href="https://brilletjes-squad-page.onrender.com" as="document">
+	<link rel="preload" href="https://brilletjes-squad-page.vercel.app/#info" as="document">
 	<link rel="preload" href="https://proof-of-concept-for-merlin.vercel.app/brew" as="document">
+	<link rel="preload" href="https://accessibilitytool.vercel.app/" as="document">
+	<link rel="preload" href="https://github.com/christoph3r3w" as="document">
+	<link rel="preload" href="https://codepen.io/Christopher-the-animator/embed/NWmxrpY?default-tab=" as="document">
 </svelte:head>
 
 <main>
 	<section id="cover-1" class="covers" >
 		<a href="#cover-2" class="frame">
 			<div class="pics-container">
-				<!-- <span class="profile profile-1">
-					<img  src="{peep.avatar}" alt="">
-				</span> -->
 				<span class="profile profile-2 flower" >
 					<img src="/25acb22a-22a3-41d5-a0eb-c91529c4c6c8.jpg" width=300 alt="image of {peep.name}">
-					<!-- <img src="https://portfolio-lti-production.s3.eu-west-1.amazonaws.com/cvq44juv5nrywjfbnlgdrwaweabo?response-content-disposition=inline%3B%20filename%3D%226992026a-4ec4-4399-a36a-c18411945cb2.JPG%22%3B%20filename%2A%3DUTF-8%27%276992026a-4ec4-4399-a36a-c18411945cb2.JPG&response-content-type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAZUHNSBLFH5JMYGQV%2F20240915%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20240915T210806Z&X-Amz-Expires=43200&X-Amz-SignedHeaders=host&X-Amz-Signature=131003c97f68d35b7ea57099e9da85018ae1decef2bad55b4a8a12c7fc12e219" width=300 alt="image of {peep.name}"> -->
 				</span>
-
 			</div>
 			
 			<h1 class="name">{peep.name} {peep.surname}</h1>
@@ -35,20 +33,16 @@
 		<a href="#cover-3" class="frame">
 			<div class="side-info scalloped-box">
 				<ul>
-					<li> squad-id {peep.squad_id}</li>
-					<li> role {peep.role}</li>
-					<li> hobbies {peep.hobbies}</li>
-					<li> and more </li>
+					<img src="chris icon lowlowres.png" alt="lil chris">
+					<li> Reading a {peep.fav_book_genre} atm</li>
+					<li> Mostly an {peep.most_energy} person</li>
 				</ul>
 			</div>
-			<div class="bio">
+			<div class="bio ">
 				{#if peep}
-				<p> {peep.bio}</p>
+					<p> {peep.bio}</p>
 				{:else}
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae rem vero nobis perferendis, quas id! Debitis nostrum, delectus culpa voluptates veritatis voluptatibus earum ratione dignissimos id magni, inventore molestiae ducimus.
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae rem vero nobis perferendis, quas id! Debitis nostrum, delectus culpa voluptates veritatis voluptatibus earum ratione dignissimos id magni, inventore molestiae ducimus.
-				</p>
+					<p class="stamp">loading...</p>
 				{/if}
 			</div>
 			<div class="side-pic">
@@ -62,13 +56,18 @@
 	<section id="cover-3" class="covers frame" >
 		
 			<section class="mainG">
-				<article class="top top_1">
+				<article class="top top_1" data-name="Merlin">
+					<a href="https://proof-of-concept-for-merlin.vercel.app/brew" aria-label="link to project"></a>
 					<iframe title="potion mixing game" src="https://proof-of-concept-for-merlin.vercel.app/brew" frameborder="0" tabindex="-1"></iframe>
+
 				</article>  
 				<article class="top top_2">2</article>
 				<article class="top top_3">3</article>
 					
-				<article class="mid mid_1">4</article>  
+				<article class="mid mid_1" data-name="A11y tool">
+				<a href="https://accessibilitytool.vercel.app/" aria-label="link to project"></a>
+					<iframe title="A11y tool concept" src="https://accessibilitytool.vercel.app/" frameborder="0" loading="lazy" tabindex="-1"></iframe>
+				</article>  
 				<article class="mid mid_2">
 					<a href="#cover-5" class="frame stamp" style="color: transparent;">
 						next
@@ -76,20 +75,18 @@
 				</article>
 				<article class="mid mid_3">6</article>
 					
-				<article class="down down_1">
+				<article class="down down_1" data-name="github">
 					<a href="https://github.com/christoph3r3w">
 						<img src="/GitHub_Logo.png" alt="github logo" width="300">
 					</a>
 				</article>  
-				<article class="down down_2">
-					<iframe title="shader" height="300" style="width: 100%;" scrolling="no" src="https://codepen.io/Christopher-the-animator/embed/NWmxrpY?default-tab=" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true" tabindex="-1">
-						See the Pen <a href="https://codepen.io/Christopher-the-animator/pen/NWmxrpY">
-						Untitled</a> by Christopher (<a href="https://codepen.io/Christopher-the-animator">@Christopher-the-animator</a>)
-						
-					  </iframe>				
+				<article class="down down_2" data-name="Shaders">
+					<a href="https://codepen.io/Christopher-the-animator/pen/NWmxrpY" aria-label="link to project"></a>	
+					<iframe title="shader experiments" src="https://codepen.io/Christopher-the-animator/embed/NWmxrpY?default-tab=" frameborder="0" loading="lazy" tabindex="-1"></iframe>			
 				</article>
-				<article class="down down_3">
-					<iframe title="brilletjes squad page" src="https://brilletjes-squad-page.onrender.com" tabindex="-1"></iframe>
+				<article class="down down_3" data-name="Windows XP">
+					<a href="https://brilletjes-squad-page.vercel.app/#info" aria-label="link to project"></a>
+					<iframe title="brilletjes squad page" src="https://brilletjes-squad-page.vercel.app/#info" tabindex="-1"></iframe>
 				</article>
 			</section> 
 		
@@ -99,18 +96,20 @@
 	</section> -->
 	<section id="cover-5" class="covers" >
 		<a href="#cover-1" class="frame">
-			
-			<ul>
-				<li>insta</li>
-				<li>github</li>
-				<li>linkedin</li>
-				<li>arena</li>
-			</ul>
-
-			<div>
-				<!-- <p>profile card semester 13</p> -->
-			</div>
+			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M21.7501 12C21.7501 12.414 21.4141 12.75 21.0001 12.75H4.81115L10.5311 18.47C10.8241 18.763 10.8241 19.238 10.5311 19.531C10.3851 19.677 10.1931 19.751 10.0011 19.751C9.80909 19.751 9.61706 19.678 9.47106 19.531L2.47106 12.531C2.40206 12.462 2.3472 12.3791 2.3092 12.2871C2.2332 12.1041 2.2332 11.8971 2.3092 11.7141C2.3472 11.6221 2.40206 11.539 2.47106 11.47L9.47106 4.46999C9.76406 4.17699 10.2391 4.17699 10.5321 4.46999C10.8251 4.76299 10.8251 5.23803 10.5321 5.53103L4.81213 11.251H21.0001C21.4141 11.25 21.7501 11.586 21.7501 12Z" fill="#2A2A2A"/>
+			</svg>
 		</a>
+		<ul>
+			<!-- <li><a style="--link-color:#E1306C;" href="#">instagram</a></li> -->
+			<li><a style="--link-color:;" href="#">instagram</a></li>
+			<li><a style="--link-color:#212830;" href={peep.website}> github</a></li>
+			<li><a style="--link-color:#0a66c2;" href="https://www.linkedin.com/in/christopher-willems-83238b196/">linkedin</a></li>
+			<li><a style="--link-color:#01940c;" href="https://www.are.na/christopher-willems/channels">are.na</a></li>
+		</ul>
+		<div>
+			<!-- <p>profile card semester 13</p> -->
+		</div>
 	</section>
 </main>
 
@@ -145,9 +144,6 @@ html,
 
 }
 
-
-
-
 main{
 	position: relative;
 	width: 100%;
@@ -169,13 +165,10 @@ main::before{
 	inset: 0;
 	z-index: -1;
 	backdrop-filter: blur(13px);
-
 }
 
-main {
-	
+main {	
 	display: flex;
-	/* flex-direction: row; */
 	background-color: rgba(212, 212, 212, 0.741);
 	overflow-x: auto;
 	overflow-y: hidden;
@@ -194,7 +187,6 @@ main {
 	scroll-snap-align:center;
 	scroll-snap-stop:always;
 	
-	/* margin-inline: 2.5px; */
 	scroll-margin-inline: 2.5px;
 	align-items: center;
     justify-content: center;
@@ -209,11 +201,11 @@ main {
 	}
 }
 
-#cover-1{--color:var(--first-color); background-color:var(--color);color: var(--color);}
-#cover-2{--color:var(--second-color); background-color:var(--color);color: var(--color);}
-#cover-3{--color:var(--third-color); background-color:var(--color);color: var(--color);}
-#cover-4{--color:var(--fourth-color); background-color:var(--color);color: var(--color);}
-#cover-5{--color:var(); background-color:var(--color);color: var(--color); outline: solid black;}
+#cover-1{--color:var(--first-color);}
+#cover-2{--color:var(--second-color);}
+#cover-3{--color:var(--third-color);}
+#cover-5{--color:var(--fourth-color);outline: solid black;}
+.covers {background-color:var(--color);color: var(--color);}
 
 .covers .frame{
 	display: grid;
@@ -279,7 +271,7 @@ main {
 #cover-2::after{content: 'about';}
 #cover-3::after{content: 'work';}
 #cover-4::after{content: 'school';}
-#cover-5::after{content: 'profile card sprint 13';}
+#cover-5::after{content: 'more';}
 
 
 /* ///////////// */
@@ -378,21 +370,20 @@ main {
 	grid-template-rows: 1fr 1fr 1.5fr;
 	color: initial;
 	mix-blend-mode:luminosity;
-
 }
 
 #cover-2:is(:global(:active,:focus-within)) img{
-		opacity: 1;
-		right: -10%;
-		top: -20%;
-		transition: 1s ease-in-out;
-		z-index: 4;
-		mix-blend-mode:darken;
+	opacity: 1;
+	right: -10%;
+	top: -20%;
+	transition: 1s ease-in-out;
+	z-index: 4;
+	mix-blend-mode:darken;
 }
 
 .side-info{
 	grid-column: 1/2;
-	grid-row: 3/-1;
+	grid-row: 3/span 1;
 	place-self: center;
 	margin: 5%;
 }
@@ -412,6 +403,26 @@ main {
 	overflow: hidden;
 	/* font-size: clamp(1rem, -12rem + 16vw, 9rem); */
 	z-index: 2;
+
+	p{
+		font-size: clamp(1rem,  2ex, 3rem);
+		line-height: 1.2;
+		/* color: blue !important; */
+		padding: 3%;
+		transition: 1s ease;
+		border-radius:70px ;
+		corner-shape: superellipse(0);
+
+	}
+
+	p:hover{
+		background-color: color-mix(in srgb, var(--second-color) 30%, rgba(255, 255, 255, 0.708));
+		background-color: color-mix(in srgb, hsl(59, 23%, 63%) 100%, rgba(255, 255, 255, 0.936));
+		background-color: color-mix(in srgb, hsl(60, 13%, 20%) 100%, rgba(255, 255, 255, 0.936));
+		backdrop-filter: blur(15px) brightness(1.7);
+		z-index: 500;
+		color: white;
+	}
 }
 
 .side-pic{
@@ -435,13 +446,14 @@ main {
 	mix-blend-mode: multiply;
 }
 
-
 .scalloped-box {
-  --r: 30px; /* radius of circles */
+  --r: 20px; 
+  border-radius: 100px;
+  corner-shape: superellipse(2);
   height: 350px;
-  aspect-ratio: 1;
-  padding: calc(1.5*var(--r));
-  background: #e97f02;
+  padding: calc(1.9 * var(--r));
+  background: #e97d02d2;
+  color: var(--second-color);
   mask: 
     linear-gradient(#000 0 0) no-repeat
      50%/calc(100% - 2*var(--r)) calc(100% - 2*var(--r)), 
@@ -482,7 +494,7 @@ main {
     1fr
     [side-end];
   
-	transition:1s;
+	transition: 1.5s linear(0, 0.003 0.4%, 0.011 0.8%, 0.026 1.3%, 0.05 1.9%, 0.097 2.9%, 0.227 5.4%, 0.292 6.7%, 0.356 8.1%, 0.416 9.5%, 0.463 10.7%, 0.506 11.9%, 0.549 13.2%, 0.588 14.5%, 0.631 16.1%, 0.673 17.8%, 0.711 19.6%, 0.746 21.4%, 0.777 23.3%, 0.806 25.3%, 0.833 27.4%, 0.858 29.7%, 0.879 32%, 0.897 34.4%, 0.915 37%, 0.929 39.7%, 0.942 42.6%, 0.953 45.7%, 0.963 49%, 0.971 52.6%, 0.983 60.5%, 0.992 70.3%, 0.996 82.7%, 1);
 	color:rgb(0, 0, 0);
 	perspective: 3000px;
 	padding:1rem;
@@ -496,19 +508,22 @@ main {
 }
 
 #cover-3 section.mainG{
+	will-change: transform, box-shadow;
 	position: relative;
 	grid-column:1/-1;
 	grid-row:1/-1;
 	display:grid;
 	grid-template-columns:subgrid;
 	grid-template-rows:subgrid;
-	transition: 1s ease-in-out;
+	transition: 1s cubic-bezier(0.8, -0.191, 0.5, 1);
 	transform-origin: right;
-	transform: rotateY(-70deg) rotateX(0deg) scale(1);
+	transform: rotateY(-50deg) rotateX(0deg) scale(1);
+	/* transform: rotateY(0deg) rotateX(70deg) scale(1); */
+	/* transform-origin: bottom; */
 	margin-left: 6%;
  
 	/* outline:solid rgb(45, 41, 41); */
-  	/* transform: rotateY(-20deg) rotateX(45deg)scale(.6); */
+  	/* transform: rotateY(-20deg) rotateX(45deg) scale(.6); */
   
   &:hover{
      transform: rotateY(0deg) rotateX(0deg)scale(1);
@@ -517,7 +532,6 @@ main {
 
 /* use @scope to controle which div you want to target */
 .mainG article:nth-of-type(n){
- 		/* inset: 20dvw; */
 		--radius:3rem;
 
 		position: relative;
@@ -525,20 +539,23 @@ main {
 	  	width:100%;
   		place-items:center;
   		font-size:3rem;
-	  	/* box-shadow: 0px 0px 50px 5px rgba(0, 0, 0, 0.351); */
 		border-radius: var(--radius);
 		opacity: .3;
 		filter: blur(2.5px) grayscale(10);
 		z-index: 0;
 		outline:solid lightslategrey;
-		/* outline:solid rgba(0, 0, 0, 0.473); */
-	  	/* grid-column: mid; */
 	
 		&:hover{
 			opacity: 1;
 			filter: blur(0);
 			z-index: 3;
+			box-shadow: 0px 0px 50px 5px rgba(0, 0, 0, 0.351);
 		}
+
+		&:not(:hover){
+			transition: .5s ease-in-out;
+			user-select: none
+ 		}
   
 }
 
@@ -547,18 +564,19 @@ main {
 	display: none;
 }
 
-.mainG article.down_3{
-	grid-column:3/-1;
-	grid-row:2/-1;
+.mainG article:has(iframe) > a{
+	position: absolute;
+	inset: 0;
+	z-index: 2;
+	/* background-color: skyblue; */
 }
 
-.mainG article.down_3 iframe{
-	position: absolute;
-	/* inset: 3%; */
-	height: 100%;
-	width: 100%;
+.mainG article :is(iframe,img){
+	border: none;
+	transition: none;
+	width: stretch;
+	height: stretch;
 	border-radius: var(--radius);
-
 }
 
 .mainG article.top_1{
@@ -568,15 +586,18 @@ main {
 
 .mainG article.top_1 iframe{
 	position: absolute;
-	/* inset: 3%; */
-	height: 100%;
-	width: 100%;
+	inset: 0;
 	border-radius: var(--radius);
-
 }
 
 .mainG article.mid_2{
 	outline: none;
+	box-shadow: none;
+}
+
+.mainG article.down_1 img{
+	width: 100%;
+	height: 100%;
 }
 
 .mainG article.down_2{
@@ -586,14 +607,16 @@ main {
 
 .mainG article.down_2 iframe{
 	border-radius: var(--radius);
-	width: auto;
-	height: 100%;
 }
 
-.mainG article.down_1 img{
-	width: 100%;
-	height: 100%;
+.mainG article.down_3{
+	grid-column:3/-1;
+	grid-row:2/-1;
+}
 
+.mainG article.down_3 iframe{
+	position: absolute;
+	border-radius: var(--radius);
 }
 
 /* .move{
@@ -605,7 +628,9 @@ main {
   	--r: 15px; /* control the radius */
 	width: 100%;
   	height: 100%;
-  	/* aspect-ratio: 1.5; */
+  	border-radius: 50px;
+	corner-shape: superellipse(-2);
+
   	padding: calc(2*var(--r));
   	background: rgba(91, 154, 216, 0.61);
   	/* outline: solid #b3b39cb8; */
@@ -615,41 +640,43 @@ main {
     linear-gradient(#000 0 0) no-repeat 50%/calc(100% - 3*var(--r)) calc(100% - 3*var(--r));
 }
 
-.box {
-  --mask: radial-gradient(40px at 40px 40px,#0000 98%,#000) -40px -40px;
-  -webkit-mask: var(--mask);
-          mask: var(--mask);
-}
 
 /* grid controls */
  #cover-3:has(:global(.top:hover,:focus-within)){
       
   	&:has(:global(.top_1:hover,:focus)){
-       grid-template-columns:5fr 1fr 3fr;
-       grid-template-rows:5fr 1fr 1fr;
+		--_bg: hsl(54, 72%, 81%);
+		background-color: var(--_bg);
+		color: var(--_bg);
+      grid-template-columns:5fr 1fr 3fr;
+      grid-template-rows:5fr 1fr 1fr;
 	   &::after{
 			content: 'Merlin';
+			color: hsl(54, 83%, 47%);
 		}
      }
    	&:has(:global(.top_2:hover)){
-		background-color: #000;
        grid-template-columns: 1fr 2.5fr 1fr;
-        grid-template-rows:2.5fr 1fr 1fr;
+       grid-template-rows:2.5fr 1fr 1fr;
      }
    	&:has(:global(.top_3:hover)){
        background-color:#EDA48D;
        grid-template-columns: 1fr  1fr 2.5fr;
        grid-template-rows:2.5fr 1fr 1fr;
-     }
-    
+     }    
 }
    
 #cover-3:has(:global(.mid:hover,:focus-within)){
     	&:has(:global(.mid_1:hover)){
+				--_bg:hsl(67, 34%, 71%);
+				background-color: var(--_bg);
+				color: var(--_bg);
        		grid-template-columns:2.5fr 1fr 1fr;
        		grid-template-rows: 1fr 2.5fr 1fr;
-
-		
+				&::after{
+					content: 'A11y tool';
+					color: inherit;
+				}	
      	}
    		&:has(:global(.mid_2:hover)){
        		background-color:rgba(0, 0, 255, 0.411);
@@ -657,12 +684,10 @@ main {
        		grid-template-rows: 1fr 1fr 1fr;
 			/* transition-property: background-color , color,grid; */
 			/* transition: .1s; */
-			
-		   	&::after{
-			content: 'Next';
-			color: aliceblue;
-			mix-blend-mode:lighten;
-
+		   &::after{
+				content: 'Next';
+				color: aliceblue;
+				mix-blend-mode:lighten;
 			}
      
      }
@@ -682,21 +707,26 @@ main {
 				}
      }
    		&:has(:global(.down_2:hover,:focus-within,:active)){
-       		/* background-color:blue; */
+       		--_bg:hsl(240, 1%, 24%);
+				background-color: var(--_bg);
+				color: var(--_bg);
        		grid-template-columns: 1fr 2.5fr 1fr;
-        	grid-template-rows: 1fr  1fr 8fr;
-			&::after{
-				content: 'Shaders';
-			}
+				grid-template-rows: 1fr  1fr 8fr;
+				&::after{
+					content: 'Shaders';
+					color: inherit;
+				}
      }
    		&:has(:global(.down_3:hover,:focus-within)){
-			background-color:#EDA48D;
+				--_bg:hsl(216, 98%, 61%);
+				background-color: var(--_bg);
+				color: var(--_bg);
        		grid-template-columns: 1fr  1fr 2.5fr;
-        	grid-template-rows: 1fr  1fr 2.5fr;
-
-		&::after{
-			content: 'windows xp';
-		}
+				grid-template-rows: 1fr  1fr 2.5fr;
+				&::after{
+					content: 'windows xp';
+					color: inherit;
+				}
      }
 }
   
@@ -704,10 +734,10 @@ main {
 #cover-3:has(:global(article:nth-of-type(n):hover)){
    
 	&article:nth-of-type(n):hover{
-	background-color:lightyellow;
-	color:transparent;
-	border-radius:5px;
-	transition: .1s;
+		background-color:lightyellow;
+		color:transparent;
+		border-radius:5px;
+		transition: .1s;
   
     &.mid_2{
       display:grid;
@@ -715,7 +745,7 @@ main {
       grid-template-rows: 1fr 1fr;
       transition: .1s;
       color:black;
-      & >div{
+      & > div{
         width:100%;
         height:100%;
       }
@@ -734,8 +764,8 @@ main {
 
 #cover-5{
 	display:grid;
-	grid-template-columns: 5fr 1fr ;
-	grid-template-rows: 1fr ;
+	grid-template-columns: 1fr 1fr 3fr 1fr ;
+	grid-template-rows: 1fr 1fr 1fr ;
 }
 
 #cover-5::after{
@@ -747,29 +777,47 @@ main {
 	display: grid;
 	grid-template-columns: subgrid;
 	grid-template-rows: subgrid;
-	grid-column: 1/-1;
-	grid-row: 1/-1;
+	grid-column: 1/-2;
+	grid-row: 1/4;
 	height: 100%;
+	/* border: yellow solid; */
 	
 }
 
 #cover-5 ul{
-	grid-column: 2/3;
-	grid-row: 1/-1;
+	grid-row: 2 / span 1;
+	grid-column: 4 / span 2;
 	display: flex;
 	flex-direction: column;
+	place-self: center;
 	place-content:end ;
+	align-items: end;
+	transform-origin: right;
 	color: #ffffffc1;
+	/* border: blue solid; */
+	width: 100%;
+	padding-right: 30%;
+
+	a{
+		color: whitesmoke;
+		text-transform: capitalize;
+	}
+
+
+	a:hover{
+		color: var(--link-color);}
 }
 
 #cover-5 div{
 	width: 100%;
 	height: 100%;
-	grid-column: 1/2;
-	grid-row: 1/-1;
+	grid-column: 3/span 1 ;
+	grid-row: 2/-1;
 	display: flex;
 	flex-wrap: wrap;
 	overflow-wrap: break-word;
+	border: red solid;
+	display: none;
 }
 
 #cover-5 div:has(:global(p)){
